38a9fc6facbb46d71a6241996c954a5e31116e5e
LouisGraup <ljgraup@gmail.com>
2025-05-29 16:21:45 +0200

    Include parameter relationships

--- Code/Julia/sensitivity_analysis.jl
+++ Code/Julia/sensitivity_analysis.jl
@@ -35,6 +35,34 @@ function sep_params(par, met)
     return par_good, par_bad
 end
 
+# function to plot parameter relationships with combined metric
+function par_plot(par, met; met_y="rmse_com", behave=true)
+    
+    # add combined metric if desired
+    if met_y == "rmse_com"
+        met.rmse_com = rmse_quad(met);
+    end
+
+    if behave
+        # only plot behavioral parameter sets
+        par, _ = sep_params(par, met);
+        met = behavioral_met(met);
+    end
+
+    np = size(par, 2);
+    par_plots = [];
+    
+    # loop through each parameter
+    for i in 1:np
+        p = scatter(par[!, i], met[!, met_y], xlabel=names(par)[i], ylabel=met_y, 
+        mc="black", msc="black", ms = 1.5, legend=false)
+        title!(p, "$(names(par)[i]) vs $(met_y)")
+        push!(par_plots, p)
+    end
+    
+    return par_plots
+end
+
 # function to calculate and plot K-S statistic for parameters
 function KS_plot(par, met)
     par_good, par_bad = sep_params(par, met);
@@ -58,9 +86,14 @@ function KS_plot(par, met)
     return ks_stat, par_plots
 end
 
-# separate parameters into behavioral and non-behavioral runs
+# function to calculate combined NSE metric
 function nse_quad(met)
     return sqrt.((met.nse10 .^ 2 + met.nse40 .^ 2 + met.nse60 .^ 2 + met.nse80 .^ 2) / 4);
+end
+
+# function to calculate combined RMSE metric
+function rmse_quad(met)
+    return sqrt.((met.rmse10 .^ 2 + met.rmse40 .^ 2 + met.rmse60 .^ 2 + met.rmse80 .^ 2) / 4);
 end
 
 density_plot(met_ctr)
@@ -73,14 +106,22 @@ met_irr_good = behavioral_met(met_irr);
 density_plot(met_ctr_good)
 density_plot(met_irr_good)
 
-# calculate quadratic mean of NSE values
-met_ctr_good.nse_com = nse_quad(met_ctr_good);
-met_irr_good.nse_com = nse_quad(met_irr_good);
-
 describe(met_ctr_good)
 
-# calculate K-S statistic to determine sensitive parameters
-ks_stat_ctr, par_plots_ctr = KS_plot(par, met_ctr);
-ks_stat_irr, par_plots_irr = KS_plot(par, met_irr);
+# compare metrics
+@df met_ctr_good scatter(:nse10, :nse40, xlabel="NSE10", ylabel="NSE40", legend=false)
+@df met_ctr_good scatter(:nse40, :nse60, xlabel="NSE40", ylabel="NSE60", legend=false)
+@df met_ctr_good scatter(:nse60, :nse80, xlabel="NSE60", ylabel="NSE80", legend=false)
+@df met_ctr_good scatter(:nse10, :nse80, xlabel="NSE10", ylabel="NSE80", legend=false)
+
+# parameter relationships
+par_plots_ctr = par_plot(par, met_ctr);
+par_plots_irr = par_plot(par, met_irr);
 
 plot(par_plots_ctr..., size=(1000,1000), layout=(3,5), legend=false, titlefontsize=8, guidefontsize=6)
+
+# calculate K-S statistic to determine sensitive parameters
+ks_stat_ctr, ks_plots_ctr = KS_plot(par, met_ctr);
+ks_stat_irr, ks_plots_irr = KS_plot(par, met_irr);
+
+plot(ks_plots_ctr..., size=(1000,1000), layout=(3,5), legend=false, titlefontsize=8, guidefontsize=6)
