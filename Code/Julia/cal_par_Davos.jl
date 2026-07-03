15a1d4746eaf198c657ba4d2db2a237f5f1d1750
LouisGraup <ljgraup@gmail.com>
2026-07-03 16:52:11 +0200

    First round Davos calibration results and sensitivity

--- Code/Julia/cal_par_Davos.jl
+++ Code/Julia/cal_par_Davos.jl
@@ -184,34 +184,34 @@ n = 1000; # number of parameter sets
 
 param = [ # store unmodified parameter ranges here for posterity
     # hydro parameters
-    ("DRAIN", 0.0, 1.0), # drainage (0, 1)
+    ("DRAIN", 0.5, 1.0), # drainage (0, 1)
     ("INFEXP", 0.0, 0.9), # infiltration exponent (0, 0.9)
     ("IDEPTH_m", 0.05, 1.0), # infiltration depth (m) (0.05, 1.0)
     # meteo parameters
     ("ALB", 0.15, 0.3), # surface albedo (0.1, 0.3)
     ("ALBSN", 0.4, 0.8), # snow surface albedo (0.4, 0.8)
     # soil parameters
-    ("RSSA", 20, 1000), # soil resistance (20, 1000)
+    ("RSSA", 100, 1000), # soil resistance (20, 1000)
     ("ths1", 0.15, 0.6), # theta_sat (0.15, 0.6)
     ("thr1", 0.0, 0.1), # theta_res (0.0, 0.1)
     ("ksat1", -0.5, 0.5), # additive factor on log10(k_sat) (-0.5, 0.5)
     ("alpha1", 0.5, 1.5), # multiplier on alpha (0.5, 1.5)
     ("npar1", 1.15, 1.3), # n (1.15, 1.3)
-    ("ths2", 0.15, 0.6), # theta_sat (0.15, 0.6)
-    ("thr2", 0.0, 0.1), # theta_res (0.0, 0.1)
+    ("ths2", 0.35, 0.55), # theta_sat (0.15, 0.6)
+    ("thr2", 0.0, 0.09), # theta_res (0.0, 0.1)
     ("ksat2", -0.5, 0.5), # additive factor on log10(k_sat) (-0.5, 0.5)
-    ("alpha2", 0.5, 1.5), # multiplier on alpha (0.5, 1.5)
-    ("npar2", 1.15, 1.3), # n (1.15, 1.3)
-    ("ths3", 0.15, 0.6), # theta_sat (0.15, 0.6)
-    ("thr3", 0.0, 0.1), # theta_res (0.0, 0.1)
-    ("ksat3", -0.5, 0.5), # additive factor on log10(k_sat) (-0.5, 0.5)
+    ("alpha2", 0.5, 1.4), # multiplier on alpha (0.5, 1.5)
+    ("npar2", 1.16, 1.3), # n (1.15, 1.3)
+    ("ths3", 0.4, 0.55), # theta_sat (0.15, 0.6)
+    ("thr3", 0.03, 0.1), # theta_res (0.0, 0.1)
+    ("ksat3", -0.4, 0.5), # additive factor on log10(k_sat) (-0.5, 0.5)
     ("alpha3", 0.5, 1.5), # multiplier on alpha (0.5, 1.5)
-    ("npar3", 1.15, 1.3), # n (1.15, 1.3)
+    ("npar3", 1.15, 1.22), # n (1.15, 1.3)
     # plant parameters
     ("CINTRL", 0.1, 0.75), # interception storage capacity per unit LAI (0.05, 0.75)
     ("FRINTLAI", 0.02, 0.2), # interception catch fraction per unit LAI (0.02, 0.2)
-    ("GLMAX", 0.001, 0.02), # stomatal conductance (0.001, 0.02)
-    ("CVPD", 0.5, 3.0), # vpd sensitivity (0.5, 3)
+    ("GLMAX", 0.001, 0.008), # stomatal conductance (0.001, 0.02)
+    ("CVPD", 0.5, 2.6), # vpd sensitivity (0.5, 3)
     ("R5", 50, 200), # radiation sensitivity (50, 200)
     ("T1", 5, 15), # low temperature threshold (5, 15)
     ("T2", 20, 35), # high temperature threshold (20, 35)
@@ -222,7 +222,7 @@ param = [ # store unmodified parameter ranges here for posterity
     ("VXYLEM_mm", 5.0, 80.0), # xylem volume (5, 80)
     ("DISPERSIVITY_mm", 30.0, 50.0), # dispersivity coefficient (30, 50)
     ("MAXROOTDEPTH", -2.0, -0.8), # max rooting depth (-2, -0.8)
-    ("BETAROOT", 0.9, 0.999) # beta root coefficient (0.9, 0.999)
+    ("BETAROOT", 0.9, 0.99) # beta root coefficient (0.9, 0.999)
 ];
 
 ### END USER INPUT ###
@@ -400,12 +400,12 @@ end_index = Dates.value(end_date - ref_date);
         maxroot = round(param_sets[root_dict["MAXROOTDEPTH"], par_id], sigdigits=4);
 
         # run model with modified root distribution
-        model = loadSPAC(cal_dir, output_prefix, simulate_isotopes = false,
+        model = loadSPAC(cal_dir, output_prefix, simulate_isotopes = true,
         root_distribution = (beta = betaroot, z_rootMax_m=maxroot));
 
     else
         # run model with input files
-        model = loadSPAC(cal_dir, output_prefix, simulate_isotopes = false)
+        model = loadSPAC(cal_dir, output_prefix, simulate_isotopes = true)
     end
 
     # model set up
---
+++ Code/Julia/sens_anal_Davos.jl
@@ -0,0 +1,176 @@
+# sensitivity analysis on LWFBrook90.jl
+
+using CSV, DataFrames, Dates
+using HypothesisTests
+using StatsPlots, Plots; gr()
+
+# function to filter out scenarios which produced error
+function filter_error(met)
+    println("Filtering out $(sum(met.swp_nse15 .== 0 .&& met.swp_nse50 .== 0 .&& met.swp_nse80 .== 0)) scenarios out of total $(size(met, 1)) which failed to run.")
+    return met[met.swp_nse15 .!= 0 .&& met.swp_nse50 .!= 0 .&& met.swp_nse80 .!= 0, :]
+end
+
+# function for density plot of metrics
+function density_plot(met)
+    density(met.swp_nse15, label="NSE15")
+    density!(met.swp_nse50, label="NSE50")
+    density!(met.swp_nse80, label="NSE80")
+    density!(met.swp_nse150, label="NSE150")
+end
+
+# function to filter metrics for behavioral runs
+function behavioral_met(met)
+
+    return(met[met.swc_nse15 .> -8.0 .&&
+               met.swc_nse50 .> -3.0 .&&
+               met.swc_nse80 .> -3.0 .&&
+               met.swc_nse150 .> -3.0 .&&
+               met.swp_nse15 .> -10.0 .&&
+               met.swp_nse50 .> -3.0 .&&
+               met.swp_nse80 .> -2.0 .&&
+               met.swp_nse150 .> -1.0 .&&
+               met.et_cor .> 0.5 .&&
+               met.et_nse .> -1.0, :])
+
+end
+
+# function to separate parameters into behavioral and non-behavioral runs
+function sep_params(par, met)
+    met_good = behavioral_met(met);
+    par_good = par[met_good.scen, :];
+    
+    met_bad = met[met.scen .∉ [met_good.scen], :];
+    par_bad = par[met_bad.scen, :];
+
+    return par_good, par_bad
+end
+
+# function to plot parameter relationships with combined metric
+function par_plot(par, met; met_y="swp_nse_com", behave=true)
+
+    if behave
+        # only plot behavioral parameter sets
+        par, _ = sep_params(par, met);
+        met = behavioral_met(met);
+    end
+
+    np = size(par, 2);
+    par_plots = [];
+    
+    # loop through each parameter
+    for i in 1:np
+        p = scatter(par[!, i], met[!, met_y], xlabel=names(par)[i], ylabel=met_y, 
+        mc="black", msc="black", ms = 1.5, legend=false)
+        title!(p, "$(names(par)[i]) vs $(met_y)")
+        push!(par_plots, p)
+    end
+    
+    return par_plots
+end
+
+# function to plot metric comparisons
+function met_plot(met, x, y)
+    
+    if isa(x, Array)
+        # create subplots
+        n = length(x);
+        met_plots = [];
+        for i in 1:n
+            p = scatter(met[!, x[i]], met[!, y[i]], xlabel=x[i], ylabel=y[i], legend=false)
+            push!(met_plots, p)
+        end
+        return plot(met_plots..., size=(1000, 1000), layout=(ceil(Int, sqrt(n)), ceil(Int, sqrt(n))))
+    else
+        return scatter(met[!, x], met[!, y], xlabel=x, ylabel=y, legend=false)
+    end
+
+end
+
+# function to calculate and plot K-S statistic for parameters
+function KS_plot(par, met)
+    par_good, par_bad = sep_params(par, met);
+    
+    np = size(par, 2);
+    ks_stat = DataFrame(par=names(par), D=zeros(np), pval=zeros(np));
+    par_plots = [];
+    
+    # loop through each parameter
+    for i in 1:np
+        ks_test = ApproximateTwoSampleKSTest(par_good[!, i], par_bad[!, i]);
+        ks_stat.D[i] = ks_test.δ; # D statistic
+        ks_stat.pval[i] = pvalue(ks_test); # p-value
+
+        p=ecdfplot(par_good[!, i], label="Behavioral")
+        ecdfplot!(p, par_bad[!, i], label="Non-behavioral")
+        title!(p, "K-S test for $(ks_stat.par[i]),\n D=$(round(ks_stat.D[i], sigdigits=3)), pval=$(round(ks_stat.pval[i], sigdigits=3))")
+        push!(par_plots, p)
+    end
+    
+    return ks_stat, par_plots
+end
+
+# function to add combined metrics to DataFrame
+function met_comb!(met)
+    met.swc_nse_com = sqrt.((met.swc_nse15 .^ 2 + met.swc_nse50 .^ 2 + met.swc_nse80 .^ 2 + met.swc_nse150 .^ 2) / 4);
+    met.swp_nse_com = sqrt.((met.swp_nse15 .^ 2 + met.swp_nse50 .^ 2 + met.swp_nse80 .^ 2 + met.swp_nse150 .^ 2) / 4);
+    met.met_com = sqrt.((met.swc_nse_com .^ 2 + met.swp_nse_com .^ 2 + met.et_nse .^ 2) / 3);
+    return nothing
+end
+
+# function to find best scenario
+function met_best_scen(met, metric=:met_com)
+    # find the index of the maximum value
+    max_idx = argmax(met[!, metric]);
+    best_scen = met.scen[max_idx];
+    
+    return best_scen, met[max_idx, :]
+end
+
+# calibration results
+met = CSV.read("LWFBcal_output/metrics_davos_20260702.csv", DataFrame);
+par = CSV.read("LWFBcal_output/param_davos_20260702.csv", DataFrame);
+
+# filter out scenarios which produced an error
+met = filter_error(met);
+
+# exploratory
+density_plot(met)
+
+# trans density plots
+density(met.et_cor, label="Corr Coef")
+density!(met.et_nse, label="NSE")
+
+density(met.max_ET)
+
+# add combined metrics
+met_comb!(met);
+
+# filter metrics for behavioral runs
+met_good = behavioral_met(met);
+println("$(size(met_good, 1)) behavioral parameter sets out of total $(size(met, 1)) in control scenario.")
+describe(met_good)
+
+density_plot(met_good)
+
+
+# compare metrics across depths
+met_plot(met_good, [:swp_nse15, :swp_nse50, :swp_nse80, :swp_nse150], [:swp_nse50, :swp_nse80, :swp_nse150, :swp_nse15])
+
+met_plot(met_good, [:swp_nse15, :swp_nse50, :swp_nse80, :swp_nse150], [:et_nse, :et_nse, :et_nse, :et_nse])
+
+# best control scenario
+scen_max, met_max = met_best_scen(met_good);
+# parameter values for the best performing scenario
+par_best = par[scen_max, :];
+par_best
+
+# parameter relationships
+par_plots = par_plot(par, met);
+
+plot(par_plots..., size=(1000,1000), layout=(5,6), legend=false, titlefontsize=8, guidefontsize=6)
+
+# calculate K-S statistic to determine sensitive parameters
+ks_stat, ks_plots = KS_plot(par, met);
+
+plot(ks_plots..., size=(1200,1200), layout=(5,8), legend=false, titlefontsize=8, guidefontsize=6)
+# behavioral is blue, non-behavioral is red
