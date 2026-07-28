bd5f0edcdbb33cfeac476bc224e191057a2f80e1
Louis Graup <ljgraup@gmail.com>
2026-07-28 16:22:58 +0200

    Calibration updates

--- Code/Julia/sens_anal_Davos.jl
+++ Code/Julia/sens_anal_Davos.jl
@@ -21,16 +21,16 @@ end
 # function to filter metrics for behavioral runs
 function behavioral_met(met)
 
-    return(met[met.swc_nse15 .> -1.75 .&&
-               met.swc_nse50 .> -1.25 .&&
-               met.swc_nse80 .> -1.25 .&&
-               met.swc_nse150 .> -1.5 .&&
-               met.swp_nse15 .> -6.25 .&&
-               met.swp_nse50 .> -1.7 .&&
-               met.swp_nse80 .> -0.25 .&&
+    return(met[met.swc_nse15 .> -1.5 .&&
+               met.swc_nse50 .> -1.2 .&&
+               met.swc_nse80 .> -1.2 .&&
+               met.swc_nse150 .> -1.35 .&&
+               met.swp_nse15 .> -6.05 .&&
+               met.swp_nse50 .> -1.55 .&&
+               met.swp_nse80 .> -0.2 .&&
                met.swp_nse150 .> 0.0 .&&
                met.et_cor .> 0.67 .&&
-               met.et_nse .> 0.15, :])
+               met.et_nse .> 0.2, :])
 
 end
 
@@ -127,8 +127,8 @@ function met_best_scen(met, metric=:met_com)
 end
 
 # calibration results
-met = CSV.read("LWFBcal_output/metrics_davos_20260721.csv", DataFrame);
-par = CSV.read("LWFBcal_output/param_davos_20260721.csv", DataFrame);
+met = CSV.read("LWFBcal_output/metrics_davos_20260722.csv", DataFrame);
+par = CSV.read("LWFBcal_output/param_davos_20260722.csv", DataFrame);
 
 # filter out scenarios which produced an error
 met = filter_error(met);
