6b858ec71eafe6d907ebd513402eb60f82bc70a6
Louis Graup <ljgraup@gmail.com>
2026-09-15 12:54:30 +0200

    Final Davos calibration

--- Code/Julia/sens_anal_Davos.jl
+++ Code/Julia/sens_anal_Davos.jl
@@ -28,9 +28,9 @@ function behavioral_met(met)
                #met.swc_nse80 .> -500.0 .&&
                met.swp_nse15 .> -5.0 .&&
                met.swp_nse50 .> -0.5 .&&
-               met.swp_nse80 .> 0.28 .&&
+               met.swp_nse80 .> 0.29 .&&
                met.et_cor .> 0.69 .&&
-               met.et_nse .> 0.27 .&&
+               met.et_nse .> 0.28 .&&
                met.max_ET .< 4.5, :])
 
 end
@@ -112,10 +112,9 @@ end
 
 # function to add combined metrics to DataFrame
 function met_comb!(met)
-    met.swc_nse_com = sqrt.((met.swc_nse15 .^ 2 + met.swc_nse50 .^ 2 + met.swc_nse80 .^ 2) / 3);
-    met.swc_rmse_com = sqrt.((met.swc_rmse15 .^ 2 + met.swc_rmse50 .^ 2 + met.swc_rmse80 .^ 2) / 3);
-    met.swp_nse_com = sqrt.((met.swp_nse15 .^ 2 + met.swp_nse50 .^ 2 + met.swp_nse80 .^ 2) / 3);
-    met.met_com = sqrt.((met.swc_nse_com .^ 2 + met.swp_nse_com .^ 2 + met.et_nse .^ 2) / 3);
+    met.swc_cor_com = sqrt.((met.swc_cor15 .^ 2 + met.swc_cor50 .^ 2 + met.swc_cor80 .^ 2) / 3);
+    met.et_com = sqrt.((met.et_cor .^ 2 + met.et_nse .^ 2) / 2);
+    met.met_com = sqrt.((met.swc_cor_com .^ 2 + met.et_com .^ 2) / 2);
     return nothing
 end
 
@@ -129,8 +128,8 @@ function met_best_scen(met, metric=:met_com)
 end
 
 # calibration results
-met = CSV.read("LWFBcal_output/metrics_davos_20260906.csv", DataFrame);
-par = CSV.read("LWFBcal_output/param_davos_20260906.csv", DataFrame);
+met = CSV.read("LWFBcal_output/metrics_davos_20260910.csv", DataFrame);
+par = CSV.read("LWFBcal_output/param_davos_20260910.csv", DataFrame);
 
 # filter out scenarios which produced an error
 met = filter_error(met);
